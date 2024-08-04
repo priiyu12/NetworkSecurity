645ba339470414fe756387fd20467585a7dd7681
priiyu12 <136467028+priiyu12@users.noreply.github.com>
2024-08-04 17:49:50 +0530

    Add files via upload

--- railFence.cpp
+++ railFence.cpp
@@ -1,8 +1,84 @@
 #include<iostream>
 #include<string>
 using namespace std;
+void decrypt() {
+    string cipherText;
+    int key;
+    string plainText;
 
-int main() {
+    cout << "Enter Cipher Text: ";
+    getline(cin, cipherText);
+
+    cout << "Enter key: ";
+    cin >> key;
+
+    if (key <= 1) {
+        cout << "Key should be greater than 1" << endl;
+        exit(0);
+    }
+
+    int length = cipherText.length();
+    char arr[key][length];
+
+    for (int i = 0; i < key; i++) {
+        for (int j = 0; j < length; j++) {
+            arr[i][j] = '*';
+        }
+    }
+
+    bool down = false;
+    int row = 0, col = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (row == 0) {
+            down = true;
+        }
+        if (row == key - 1) {
+            down = false;
+        }
+
+        arr[row][col++] = '?';
+
+        if (down) {
+            row++;
+        } else {
+            row--;
+        }
+    }
+
+    int index = 0;
+    for (int i = 0; i < key; i++) {
+        for (int j = 0; j < length; j++) {
+            if (arr[i][j] == '?' && index < length) {
+                arr[i][j] = cipherText[index++];
+            }
+        }
+    }
+
+    down = true;
+    row = 0;
+    col = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (row == 0) {
+            down = true;
+        }
+        if (row == key - 1) {
+            down = false;
+        }
+
+        plainText += arr[row][col++];
+
+        if (down) {
+            row++;
+        } else {
+            row--;
+        }
+    }
+
+    cout << "Plain Text: " << plainText << endl;
+}
+void encrypt() {
     string plainText;
     int key;
     string cipherText;
@@ -15,7 +91,7 @@ int main() {
 
     if (key <= 1) {
         cout << "Key should be greater than 1" << endl;
-        return 1;
+        exit(0);
     }
 
     int length = plainText.length();
@@ -53,6 +129,26 @@ int main() {
     }
 
     cout << "Cipher Text: " << cipherText << endl;
+}
+
+int main()
+{
+    int ch;
+    cout<<"Choose : "<<endl<<"1. Encryption\t2. Decryption"<<endl;
+    cin>>ch;
+    cin.ignore();
+    switch(ch){
+        case 1:
+            encrypt();
+            break;
+
+        case 2:
+            decrypt();
+            break;
+
+        default :
+            cout<<"Invalid choice!";
+    }
 
     return 0;
 }
